0407463c4186495f3a9f683dfc38b6f545fdddc7
thecoder-001 <ayush06feb@gmail.com>
2025-02-22 23:45:10 +0530

    Consistent back button
    Signed-off-by: thecoder-001 <ayush06feb@gmail.com>

--- app/editorial/edit-course/edit-form/[courseName]/page.js
+++ app/editorial/edit-course/edit-form/[courseName]/page.js
@@ -1,19 +1,21 @@
 import CourseForm from "@/components/CourseForm";
+import { ArrowLeft } from "lucide-react";
 import Link from "next/link";
 
 export default function EditCoursePage({ params }) {
   return (
     <div className="flex flex-col min-h-screen p-6">
-      <h1 className="text-4xl font-bold mb-4">Edit Course</h1>
+      <div className="max-w-lg mb-4">
+        <Link
+          href="/editorial/edit-hub"
+          className="inline-flex items-center px-4 py-2 text-sm font-medium text-blue-600 hover:text-blue-800"
+        >
+          <ArrowLeft className="w-4 h-4 mr-2" />
+          Back to Editorial Hub
+        </Link>
+      </div>
       <div className="flex-grow">
         <CourseForm courseCode={params.courseName} />
-      </div>
-      <div className="mt-8 mb-4">
-        <Link href="/editorial/edit-course">
-          <p className="text-blue-400 hover:text-blue-600 text-xl">
-            Back to Course Selection
-          </p>
-        </Link>
       </div>
     </div>
   );
--- components/CourseForm.js
+++ components/CourseForm.js
@@ -478,8 +478,8 @@ const CourseForm = ({ courseCode }) => {
   );
 
   return (
-    <div className="max-w-4xl mx-auto p-4 bg-white rounded-lg shadow-md">
-      <h1 className="text-xl font-bold mb-4">
+    <div className="max-w-4xl mx-auto p-5 bg-white rounded-lg shadow-md">
+      <h1 className="text-2xl font-bold mb-6 text-gray-800">
         {isEditMode ? "Edit Course" : "Create New Course"}
       </h1>
 
